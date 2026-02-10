22a8941076b73aaf389bfaed33977e9703fda086
saikirangobbaka <159161538+saikirangobbaka@users.noreply.github.com>
2026-02-10 15:56:52 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Name is changes here mawa");
+        System.out.println("webHooks work ayyayi raa mawa");
     }
 }   
+
 
 
 
